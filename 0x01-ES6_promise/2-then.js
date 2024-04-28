baf14dedaf2cf8b6db2be7c7b1311b568f6fae31
Evance Odoyo <odoyoevanc@gmail.com>
2024-04-29 00:42:23 +0300

    fix the function passed to finally

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,6 @@
-function onFulfilled() {
-  return { status: 200, body: 'success' };
-}
-
-function onRejected() {
-  return new Error();
-}
-
 export default function handleResponseFromAPI(promise) {
-  promise.then(onFulfilled, onRejected).finally(() => { console.log('Got a response from the API'); });
+  promise.then(
+    () => ({ status: 200, body: 'success' }),
+    () => new Error(),
+  ).finally(() => { console.log('Got a response from the API'); });
 }
