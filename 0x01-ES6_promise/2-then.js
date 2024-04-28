501790b3d05ed3a34a18c5bb033cc0c2cfe8c306
Evance Odoyo <odoyoevanc@gmail.com>
2024-04-29 00:37:09 +0300

    thenable and finally

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,11 @@
+function onFulfilled() {
+  return { status: 200, body: 'success' };
+}
+
+function onRejected() {
+  return new Error();
+}
+
 export default function handleResponseFromAPI(promise) {
-  promise.then(
-    () => ({ status: 200, body: 'success' }),
-    () => new Error(),
-  ).finally(
-    console.log('Got a response from the API'),
-  );
+  promise.then(onFulfilled, onRejected).finally(() => { console.log('Got a response from the API'); });
 }
