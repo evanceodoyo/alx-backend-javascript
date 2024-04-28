33ac679bc556680ba1897ec31efb1ff956feefa6
Evance Odoyo <odoyoevanc@gmail.com>
2024-04-29 01:18:23 +0300

    add return statement for the promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  promise
+  return promise
     .then(() => ({ status: 200, body: 'success' }))
-    .catch(() => {
-      throw new Error();
-    })
+    .catch(() => new Error())
     .finally(() => { console.log('Got a response from the API'); });
 }
