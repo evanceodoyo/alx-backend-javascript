cdcd7640ac813396c62318e30a452041e529c9ea
Evance Odoyo <odoyoevanc@gmail.com>
2024-04-30 21:33:35 +0300

    use dot notation

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,8 +3,8 @@ import { uploadPhoto, createUser } from './utils.js';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
-      const valuesObj = {...values[0], ...values[1]}
-      console.log(`${valuesObj['body']} ${valuesObj['firstName']} ${valuesObj['lastName']}`);
+      const valuesObj = { ...values[0], ...values[1] };
+      console.log(`${valuesObj.body} ${valuesObj.firstName} ${valuesObj.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
