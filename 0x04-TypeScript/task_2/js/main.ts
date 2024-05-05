1dd4314db56c34f0131d7906d50341af8afa91da
Evance Odoyo <odoyoevanc@gmail.com>
2024-05-06 01:42:15 +0300

    string literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -60,9 +60,24 @@ function executeWork(employee: Director | Teacher) {
     }
 }
 
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    }
+
+    if (todayClass === 'History') {
+        return 'Teaching History';
+    }
+}
+
 console.log(createEmployee(200)); // Teacher
 console.log(createEmployee(1000)); // Director
 console.log(createEmployee('$500')); // Director
 
 console.log(executeWork(createEmployee(200))); // Getting to work
 console.log(executeWork(createEmployee(1000))); // Getting to director tasks
+
+console.log(teachClass('Math')); // Teaching Math
+console.log(teachClass('History')); // Teaching History
