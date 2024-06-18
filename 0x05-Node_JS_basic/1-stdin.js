0e0e61289aa4aa19698d16d34b7d4e85ca2de121
Evance Odoyo <odoyoevanc@gmail.com>
2024-06-18 23:37:00 +0300

    Correct typo in name

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,11 @@
+process.stdin.setEncoding('utf8');
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
-  if (input) {
-    process.stdout.write(`Your names is: ${input}`);
+  if (input !== null) {
+    process.stdout.write(`Your name is: ${input}`);
   }
 });
 
