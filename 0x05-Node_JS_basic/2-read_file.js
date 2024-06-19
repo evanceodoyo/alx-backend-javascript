e5d81ca2edfd1ff3fcfd0e65d3a69be37827293d
Evance Odoyo <odoyoevanc@gmail.com>
2024-06-19 06:03:16 +0300

    Read a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,33 @@
+const fs = require('node:fs');
+
+function countStudents (path) {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+    const rows = data.trim().split('\n').slice(1);
+    const columns = [];
+    for (let i = 0; i < rows.length; i++) {
+      columns.push(rows[i].split(','));
+    }
+
+    const studentFields = {};
+    for (let col = 0; col < columns.length; col++) {
+      const field = columns[col][columns[col].length - 1];
+      const studentName = columns[col][0];
+
+      if (!(field in studentFields)) {
+        studentFields[field] = [studentName];
+      } else {
+        studentFields[field].push(studentName);
+      }
+    }
+    console.log(`Number of students: ${rows.length}`);
+    for (const fld in studentFields) {
+      const studentList = studentFields[fld];
+      console.log(`Number of students in ${fld}: ${studentList.length}. List: ${studentList.join(', ')}`);
+    }
+  });
+}
+
+module.exports = countStudents;
