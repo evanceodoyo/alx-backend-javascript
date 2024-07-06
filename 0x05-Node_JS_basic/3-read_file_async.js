42836c2631ac7dcb5eee3db5ac915c9c7d3abee5
Evance Odoyo <odoyoevanc@gmail.com>
2024-07-07 01:41:49 +0300

    reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+const fs = require('fs/promises');
+
+const countStudents = async (path) => {
+  fs.readFile(path)
+    .then((data) => {
+      const fileLines = data.toString('utf-8').trim().split('\n');
+      const studentsGroupedByFields = {};
+      const dbFieldNames = fileLines[0].split(',');
+      const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
+
+      for (const line of fileLines.slice(1)) {
+        const studentRecord = line.split(',');
+        const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+        const field = studentRecord[studentRecord.length - 1];
+        if (!Object.keys(studentsGroupedByFields).includes(field)) {
+          studentsGroupedByFields[field] = [];
+        }
+        const studentEntries = studentPropNames
+          .map((propName, idx) => [propName, studentPropValues[idx]]);
+        studentsGroupedByFields[field].push(Object.fromEntries(studentEntries));
+      }
+
+      const totalStudents = Object
+        .values(studentsGroupedByFields)
+        .reduce((pre, cur) => (pre || []).length + cur.length);
+      console.log(`Number of students: ${totalStudents}`);
+      for (const [field, group] of Object.entries(studentsGroupedByFields)) {
+        const studentNames = group.map((student) => student.firstname).join(', ');
+        console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+      }
+    })
+    .catch((_err) => { throw new Error('Cannot load database'); });
+};
+
+module.exports = countStudents;
