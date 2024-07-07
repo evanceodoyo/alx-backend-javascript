a5430f97466a554f845b304efbc10787e3d750a6
Evance Odoyo <odoyoevanc@gmail.com>
2024-07-07 03:39:35 +0300

    more complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,56 @@
+const fs = require('fs').promises;
+const express = require('express');
+
+const app = express();
+
+const countStudents = async (path = 'database.csv') => {
+  try {
+    const data = await fs.readFile(path, 'utf-8');
+    const fileLines = data.trim().split('\n');
+    const studentsGroupedByFields = {};
+    const dbFieldNames = fileLines[0].split(',');
+    const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
+
+    for (const line of fileLines.slice(1)) {
+      const studentRecord = line.split(',');
+      const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+      const field = studentRecord[studentRecord.length - 1];
+      if (!Object.keys(studentsGroupedByFields).includes(field)) {
+        studentsGroupedByFields[field] = [];
+      }
+      const studentEntries = studentPropNames
+        .map((propName, idx) => [propName, studentPropValues[idx]]);
+      studentsGroupedByFields[field].push(Object.fromEntries(studentEntries));
+    }
+
+    const totalStudents = Object
+      .values(studentsGroupedByFields)
+      .reduce((pre, cur) => (pre || []).length + cur.length);
+    let result = `Number of students: ${totalStudents}\n`;
+    for (const [field, group] of Object.entries(studentsGroupedByFields)) {
+      const studentNames = group.map((student) => student.firstname).join(', ');
+      result += `Number of students in ${field}: ${group.length}. List: ${studentNames}\n`;
+    }
+
+    return result.trim();
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+};
+
+app.get('/', (_req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (_req, res) => {
+  try {
+    const result = await countStudents(process.argv[2]);
+    res.send(`This is the list of our students\n${result}`);
+  } catch (err) {
+    res.send(err.message);
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
