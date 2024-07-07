c0a6a8f32f72028a7da62cfecb77262abdb72f1d
Evance Odoyo <odoyoevanc@gmail.com>
2024-07-07 05:14:12 +0300

    full server

---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,44 @@
+import readDatabase from '../utils';
+
+export default class StudentsController {
+  static async getAllStudents(request, response) {
+    try {
+      const result = await readDatabase('./database.csv');
+      const sortedKeys = Object.keys(result)
+        .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+
+      const sortedResult = {};
+      sortedKeys.forEach((key) => {
+        sortedResult[key] = result[key];
+      });
+
+      let res = 'This is the list of our students\n';
+
+      for (const [field, group] of Object.entries(sortedResult)) {
+        const studentNames = group.map((student) => student.firstname).join(', ');
+        res += `Number of students in ${field}: ${group.length}. List: ${studentNames}\n`;
+      }
+
+      return response.status(200).send(res);
+    } catch (err) {
+      return response.status(500).send(err.message);
+    }
+  }
+
+  static async getAllStudentsByMajor(request, response) {
+    try {
+      const { major } = request.params;
+
+      if (!major || !['CS', 'SWE'].includes(major)) {
+        return response.status(500).send('Major parameter must be CS or SWE');
+      }
+
+      const result = await readDatabase('./database.csv');
+      const names = result[major].map((student) => student.firstname).join(', ');
+
+      return response.status(200).send(`List: ${names}`);
+    } catch (err) {
+      return response.status(500).send(err.message);
+    }
+  }
+}
---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+const readDatabase = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const fileLines = data.toString('utf-8').trim().split('\n');
+    const studentsGroupedByFields = {};
+    const dbFieldNames = fileLines[0].split(',');
+    const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
+
+    for (const line of fileLines.slice(1)) {
+      const studentRecord = line.split(',');
+      const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+      const field = studentRecord[studentRecord.length - 1];
+      if (!Object.keys(studentsGroupedByFields).includes(field)) {
+        studentsGroupedByFields[field] = [];
+      }
+      const studentEntries = studentPropNames
+        .map((propName, idx) => [propName, studentPropValues[idx]]);
+      studentsGroupedByFields[field].push(Object.fromEntries(studentEntries));
+    }
+
+    resolve(studentsGroupedByFields);
+  });
+});
+
+module.exports = readDatabase;
