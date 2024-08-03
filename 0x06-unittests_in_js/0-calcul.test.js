080b4d3ef777aa5166038a8ff6e1c2130b3c64e6
Evance Odoyo <odoyoevanc@gmail.com>
2024-08-04 01:08:42 +0300

    properly organise the test suites

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,24 +2,24 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', function () {
-    it('should return 4 when if `a = 1` and `b = 3`', function () {
-        assert.equal(calculateNumber(1, 3), 4);
+    it('should round first argument', function () {
+        assert.equal(calculateNumber(1.0, 3), 4);
+        assert.equal(calculateNumber(1.3, 3), 4);
+        assert.equal(calculateNumber(1.7, 3), 5);
     });
 
-    it('should return 5 when if `a = 1` [rounded] and `b = 3.7` [rounded]', function () {
+    it('should round the second argument', function () {
         assert.equal(calculateNumber(1, 3.7), 5);
+        assert.equal(calculateNumber(1, 3.3), 4);
+        assert.equal(calculateNumber(0, 3.0), 3);
     });
 
-    it('should return 5 when if `a = 1.5` [rounded] and `b = 3` [rounded]', function () {
-        assert.equal(calculateNumber(1.5, 3), 5);
-    });
-
-
-    it('should return 5 when if `a = 1.2` [rounded] and `b = 3.7 [rounded]`', function () {
-        assert.equal(calculateNumber(1.2, 3.7), 5);
-    });
-
-    it('should return 6 when if `a = 1.5` [rounded] and `b = 3.7` [rounded]', function () {
-        assert.equal(calculateNumber(1.5, 3.7), 6);
+    it('should return return the right number', function () {
+        assert.equal(calculateNumber(1.3, 0), 1);
+        assert.equal(calculateNumber(0, 1.2), 1);
+        assert.equal(calculateNumber(1.3, 1.3), 2);
+        assert.equal(calculateNumber(1.7, 1.2), 3);
+        assert.equal(calculateNumber(1.3, 1.8), 3);
+        assert.equal(calculateNumber(1.6, 1.8), 4);
     });
 });
