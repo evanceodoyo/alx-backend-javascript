7b836c5746170cd5058bd5df4f1dc8135483c808
Evance Odoyo <odoyoevanc@gmail.com>
2024-08-04 21:25:02 +0300

    fix: restore a spy after using it

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,8 +4,14 @@ const Utils = require("./utils.js");
 const sendPaymentRequestToApi = require("./3-payment.js");
 
 describe("sendPaymentRequestToApi", function () {
+  let spy;
+
+  afterEach(function () {
+    spy.restore();
+  });
+
   it("should call Utils.calculateNumber with correct arguments", function () {
-    const spy = sinon.spy(Utils, "calculateNumber");
+    spy = sinon.spy(Utils, "calculateNumber");
     sendPaymentRequestToApi(100, 20);
     expect(spy.calledOnce).to.be.true;
     expect(spy.firstCall.args).to.deep.equal(["SUM", 100, 20]);
